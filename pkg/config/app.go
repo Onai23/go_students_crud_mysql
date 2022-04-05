d6f3264db72d246ec70176c832277b7c3d904b1f
Onai23 <onai.nadapdap.23@gmail.com>
2022-04-05 14:02:30 +0700

    Modifikasi file app.go koneksi database

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -7,3 +7,14 @@ var (
 )
 
 //set fungsi connect
+func Connect() {
+	d, err := gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/db_go_mysql?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
