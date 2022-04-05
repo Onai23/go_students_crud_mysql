813011f103d7d9edf7cce021e542a1c81f7dc1b7
Onai23 <onai.nadapdap.23@gmail.com>
2022-04-05 14:05:36 +0700

    Modifikasi file utils.go

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
+//set fungsi ParseBody()
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := ioutil.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return
+		}
+	}
 }
